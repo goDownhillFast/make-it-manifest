c390312aafce7b4cff4faa24ef3b022f4b92bda3
jondayton <jondayton@gmail.com>
2013-07-14 20:56:11 -0400

    added selectors for hymns and people

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,28 +1,40 @@
 $(function () {
   $('.chzn-select').chosen();
 
-  $('.draggable').draggable({helper:'clone'});
+  $('.chzn-select').change(function () {
+    var selected = $(this).find(":selected")
+    var modified = $(this).closest('.line').find('.modified-item');
+    modified.find('a').attr('href', selected.attr("link"));
+    modified.find('.modified-name').append(selected.text());
+    if (modified.find('.modified-value')) {
+      modified.find('.modified-value').append(selected.attr('value'));
+    }
+    $(this).closest('.choose-item').remove();
+    modified.fadeIn();
+  });
+
+  $('.draggable').draggable({helper: 'clone'});
 
 
   droppableProgramElt($(".droppable"));
 
-  $( ".sortable-option-list" ).sortable({
+  $(".sortable-option-list").sortable({
     connectWith: ".connectedSortable",
     forcePlaceholderSize: false,
-    helper: function(e,li) {
-      copyHelper= li.clone();
+    helper: function (e, li) {
+      copyHelper = li.clone();
       droppableProgramElt(copyHelper);
       copyHelper.insertAfter(li);
       return li.clone();
     },
-    stop: function() {
+    stop: function () {
       copyHelper && copyHelper.remove();
     }
   });
 
   $(".connectedSortable").sortable({
-    receive: function(e,ui) {
-      copyHelper= null;
+    receive: function (e, ui) {
+      copyHelper = null;
     }
   });
 
@@ -32,6 +44,7 @@ function droppableProgramElt(elt) {
   elt.droppable({
     drop: function (ev, ui) {
       var dragged = $(ui.draggable.context).clone();
+      $(this).find('.draggable').remove();
       $(this).append(dragged);
       //      console.log(ui);
 //      console.log(ui.draggable.context);
