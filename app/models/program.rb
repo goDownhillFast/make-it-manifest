c35e7053340494730b2655b9a2aa5ef40d641bc1
jondayton <jondayton@gmail.com>
2013-12-09 09:37:58 -0500

    default date calc

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,10 +1,18 @@
 class Program < ActiveRecord::Base
   validates_presence_of :body_html
 
-  attr_accessible :announcements, :program_week, :body_html, :cover_pic_url
+  before_create :calc_date
+
+  attr_accessible :announcements, :announcements2, :program_week, :body_html, :cover_pic_url
 
   def cover_pic
     cover_pic_url || "new-testament/jesus-raising-lazurus-634865"
+  end
+
+  protected
+
+  def calc_date
+   self.program_week = Date.today.sunday
   end
 
 end
